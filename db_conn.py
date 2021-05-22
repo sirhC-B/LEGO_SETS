c18b861135a1cef3fa89ce9b907c3681eade7298
jakob <jakobkahf@gmail.com>
2021-05-22 13:00:29 +0200

    db_conn.py: Verbindungsaufbau zum PostgresSQL

---
+++ db_conn.py
@@ -0,0 +1,21 @@
+from tkinter import Tk, messagebox
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+ENDPOINT = "lego-oos.cwe1ewbx6zz5.us-east-1.rds.amazonaws.com"
+PORT = "5432"
+USR = "postgresOOS"
+REGION = "us-east-1"
+DBNAME = "postgresSQL"
+load_dotenv()
+PASS = os.environ.get('PASS')
+try:
+    db = psycopg2.connect(host=ENDPOINT, port=PORT, database=DBNAME, user=USR, password=PASS)
+
+except Exception as e:
+    rootWin = Tk()
+    rootWin.withdraw()
+    messagebox.showerror("Fehler", "Fehler beim Verbindungsaufbau zur Datenbank aufgrund von {}".format(e),
+                         parent=rootWin)
+    rootWin.destroy()
