193d19ec922c91dc6406d7a2531b74d320b7fd8c
sirhC-B <chris_boesener@gmx.net>
2021-05-24 16:41:24 +0200

    + Suchfunktion in purchaseTable

--- LEGO.py
+++ LEGO.py
@@ -181,7 +181,6 @@ class UserInterface:
         self.textInfo = Text(self.footerFrame, height=7, width=90)
         self.textInfo.grid(row=1, padx=(20,10), pady=5)
 
-
         ########### SEARCH ############
         def search(var):
             query = self.searchentry.get()
